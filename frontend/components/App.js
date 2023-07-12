a2397d3bba99c7e691ddac033eef58b29c304347
taylersala <taylersala@gmail.com>
2023-07-12 14:41:23 -0700

    18 min

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+
+fetchAllTodos = () => {
+  axios.get(URL)
+    .then(res => {
+      this.setState({ ...this.state, todos: res.data.data })
+    }) 
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+  componentDidMount() {
+    //FETCH All todos from server
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error: No Error Found</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input tyoe="text" placeholder="Type Todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
+
   }
 }
