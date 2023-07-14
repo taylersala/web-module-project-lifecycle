9a3bfdec5b8310e34b5b0bd3016f5b1f6171ad00
taylersala <taylersala@gmail.com>
2023-07-14 11:49:18 -0700

    completed

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    return (
+      <div 
+      onClick={this.props.toggleCompleted(this.props.todo.id)} 
+      key={this.props.todo.id}
+      >
+        {this.props.todo.name} 
+        {this.props.todo.completed ? ' ✔️': ''}
+      </div>
+    )
   }
 }
